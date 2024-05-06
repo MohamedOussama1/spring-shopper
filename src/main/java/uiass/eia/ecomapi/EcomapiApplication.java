60f65f1736b5d692eec0853a24ce4ddca75f78d2
MohamedOussama1 <115744755+MohamedOussama1@users.noreply.github.com>
2024-05-06 10:16:33 +0200

    Update EcomapiApplication.java

--- src/main/java/uiass/eia/ecomapi/EcomapiApplication.java
+++ src/main/java/uiass/eia/ecomapi/EcomapiApplication.java
@@ -13,7 +13,7 @@ public class EcomapiApplication implements CommandLineRunner {
     IServiceMetier serviceMetier;
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Test!");
         SpringApplication.run(EcomapiApplication.class, args);
 
     }
